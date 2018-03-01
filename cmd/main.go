65e162c8956b04611ad40fee76eb0c4ca019e4c8
Quy Le Ngoc <lnquy.it@gmail.com>
2018-03-01 18:01:10 +0700

    Frappe heatmap. WIP

--- cmd/main.go
+++ cmd/main.go
@@ -10,13 +10,8 @@ import (
 	"github.com/lnquy/argus/lib/github"
 )
 
-type Report struct {
-	Date    time.Time `json:"date"`
-	Commits int       `json:"commits"`
-}
-
 var (
-	contribs map[string]int
+	contribs map[int64]int
 )
 
 func main() {
@@ -34,14 +29,14 @@ func main() {
 	b, _ := json.Marshal(repos)
 	fmt.Printf("Raw repos: \n%s", string(b))
 
-	contribs = make(map[string]int)
+	contribs = make(map[int64]int)
 	for _, r := range repos {
 		for _, c := range r.Commits {
-			d := c.Date.Format("2006-01-02")
-			if v, ok := contribs[d]; !ok {
-				contribs[d] = 1
+			ts := c.Date.Unix() - (c.Date.Unix() % 86400) // To midnight
+			if v, ok := contribs[ts]; !ok {
+				contribs[ts] = 1
 			} else {
-				contribs[d] = v + 1
+				contribs[ts] = v + 1
 			}
 		}
 	}
@@ -50,10 +45,9 @@ func main() {
 	now := time.Now().Unix()
 	today := now - (now % 86400) // To midnight ts
 	for i := 0; i < 375; i++ {
-		ds := time.Unix(today, 0).Format("2006-01-02")
-		log.Printf("date: %s\n", ds)
-		if _, ok := contribs[ds]; !ok {
-			contribs[ds] = 0
+		log.Printf("date: %s\n", today)
+		if _, ok := contribs[today]; !ok {
+			contribs[today] = 0
 		}
 		today -= 86400
 	}
